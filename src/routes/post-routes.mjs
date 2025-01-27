05e64974f7703272d70c360bab434594e6858aa2
Samara Souza <samyesouza13@gmail.com>
2025-01-27 11:48:36 -0300

    integrating gemini api to backend

--- src/routes/post-routes.mjs
+++ src/routes/post-routes.mjs
@@ -7,3 +7,4 @@ publicRouter.get('/posts', (req, res) => postController.getPosts(req, res));
 privateRouter.post('/posts', (req, res) => postController.createPost(req, res));
 privateRouter.put('/posts/:id', (req, res) => postController.updatePost(req, res));
 privateRouter.delete('/posts/:id', (req, res) => postController.deletePost(req, res));
+privateRouter.post('/suggestions',(req, res) => postController.suggestionsAI(req,res));
